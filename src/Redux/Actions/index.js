2ee4aeb0ff85a9d1339c6d63881bcccebacca573
AraicheKhalil <ucandevlopeit@gmail.com>
2024-07-02 02:34:47 +0100

    fix broken

--- src/Redux/Actions/index.js
+++ src/Redux/Actions/index.js
@@ -5,7 +5,7 @@ export const scanDocument = async (payload) => {
     const formData = new FormData();
     formData.append(`files`, payload);
     const response = await axios.post(
-      "https://dsfsmd.fly.dev/process-document",
+      "http://dsfsmd-container.eastus.azurecontainer.io:8000/process-document/",
       formData,
       {
         headers: { "Content-Type": "multipart/form-data" },
